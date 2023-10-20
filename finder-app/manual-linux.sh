cf710e02239dcdb87797753f67898a988f03ebe0
fariadif <fabiofariadias@hotmail.com>
2023-10-20 09:11:14 -0300

    Bump changeS

--- finder-app/manual-linux.sh
+++ finder-app/manual-linux.sh
@@ -2,6 +2,8 @@
 # Script outline to install and build kernel.
 # Author: Siddhant Jajoo.
 
+# ./finder-app/manual-linux.sh /home/fabio/Documents/studies/linux-studies/linux-assignment-3-part-2/aeld
+
 set -e
 set -u
 
@@ -21,22 +23,36 @@ else
 	echo "Using passed directory ${OUTDIR} for output"
 fi
 
-mkdir -p ${OUTDIR}
+mkdir -p ${OUTDIR} && cd "$OUTDIR"
 
-cd "$OUTDIR"
 if [ ! -d "${OUTDIR}/linux-stable" ]; then
     #Clone only if the repository does not exist.
 	echo "CLONING GIT LINUX STABLE VERSION ${KERNEL_VERSION} IN ${OUTDIR}"
-	git clone ${KERNEL_REPO} --depth 1 --single-branch --branch ${KERNEL_VERSION}
+	git clone ${KERNEL_REPO} ${OUTDIR} --depth 1 --single-branch --branch ${KERNEL_VERSION}
 fi
+
 if [ ! -e ${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image ]; then
     cd linux-stable
     echo "Checking out version ${KERNEL_VERSION}"
     git checkout ${KERNEL_VERSION}
+    echo "Changing file ${OUTDIR}/linux-stable/scripts/dtc/dtc-lexer.lex.c"
+    # sed -i 's/YYLTYPE yylloc/extern &/' ${OUTDIR}/linux-stable/scripts/dtc/dtc-lexer.lex.c
+    # git apply ./patch.diff ${OUTDIR}/${KERNEL_REPO}/scripts/dtc/dtc-lexer.lex.c
+    # git apply ./patch.diff
+    # YYLTYPE yylloc
+    # 
+    # /home/fabio/Documents/studies/linux-studies/linux-assignment-3-part-2/aeld/linux-stable/scripts/dtc/dtc-lexer.lex.c  
+    # git diff --no-index /path/to/foo /path/to/bar
+
 
     # TODO: Add your kernel build steps here
+    echo $PATH
+    #make -j4 ARCH=arm64 CROSS_COMPILE=aarch64-none-linux-gnu- mrproper
+    sed -i 's/YYLTYPE yylloc/extern &/' ${OUTDIR}/linux-stable/scripts/dtc/dtc-lexer.lex.c
+    make -j4 ARCH=arm64 CROSS_COMPILE=aarch64-none-linux-gnu- defconfig
+    make -j4 ARCH=arm64 CROSS_COMPILE=aarch64-none-linux-gnu- all
 fi
-
+#exit 1
 echo "Adding the Image in outdir"
 
 echo "Creating the staging directory for the root filesystem"
@@ -48,27 +64,44 @@ then
 fi
 
 # TODO: Create necessary base directories
+mkdir -p "${OUTDIR}/rootfs/bin/"
+mkdir -p "${OUTDIR}/rootfs/dev/"
+mkdir -p "${OUTDIR}/rootfs/etc/"
+mkdir -p "${OUTDIR}/rootfs/lib/"
+mkdir -p "${OUTDIR}/rootfs/proc/"
+mkdir -p "${OUTDIR}/rootfs/sys/"
+mkdir -p "${OUTDIR}/rootfs/sbin/"
+mkdir -p "${OUTDIR}/rootfs/tmp/"
+mkdir -p "${OUTDIR}/rootfs/usr/bin/"
+mkdir -p "${OUTDIR}/rootfs/usr/sbin/"
+mkdir -p "${OUTDIR}/rootfs/var/"
 
 cd "$OUTDIR"
 if [ ! -d "${OUTDIR}/busybox" ]
 then
-git clone git://busybox.net/busybox.git
-    cd busybox
-    git checkout ${BUSYBOX_VERSION}
+    git clone git://busybox.net/busybox.git
+    cd ${OUTDIR}/busybox && git checkout ${BUSYBOX_VERSION}
     # TODO:  Configure busybox
+    make -j4 ARCH=arm64 CROSS_COMPILE=aarch64-none-linux-gnu- defconfig
 else
     cd busybox
 fi
 
 # TODO: Make and install busybox
+make -j4 ARCH=arm64 CROSS_COMPILE=aarch64-none-linux-gnu-
+#busybox --install -s "${OUTDIR}/rootfs/bin/"
+make -j4 ARCH=arm64 CROSS_COMPILE=aarch64-none-linux-gnu- CONFIG_PREFIX="${OUTDIR}/rootfs/" install
 
+chmod 700 ${OUTDIR}/rootfs/bin/busybox
+
+
+# TODO: Add library dependencies to rootfs
 echo "Library dependencies"
 ${CROSS_COMPILE}readelf -a bin/busybox | grep "program interpreter"
 ${CROSS_COMPILE}readelf -a bin/busybox | grep "Shared library"
 
-# TODO: Add library dependencies to rootfs
-
 # TODO: Make device nodes
+mkdir -p ${OUTDIR}/rootfs/dev
 
 # TODO: Clean and build the writer utility
 
@@ -76,5 +109,6 @@ ${CROSS_COMPILE}readelf -a bin/busybox | grep "Shared library"
 # on the target rootfs
 
 # TODO: Chown the root directory
+chown root:root ${OUTDIR}/rootfs
 
 # TODO: Create initramfs.cpio.gz
